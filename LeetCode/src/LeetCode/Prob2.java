345c006b59ed6340250a53064ecf4e0ad130ddde
Kephael <me@caseybaer.com>
2016-01-09 18:08:18 -0500

    Cleaned up problem 2

--- LeetCode/src/LeetCode/Prob2.java
+++ LeetCode/src/LeetCode/Prob2.java
@@ -1,17 +1,22 @@
+/*
+ * You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *  Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ */
+
 package LeetCode;
 
-  class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) { val = x; }
-  }
- 
- public class Prob2 {
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) { val = x; }
+}
+
+public class Prob2 {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		ListNode first = null;
 		int carry = 0;
 		ListNode ptr = null;
-		boolean init = true;
 		while (l1 != null || l2 != null || carry != 0) {
 			int val = 0;
 			if (l1 != null) {
@@ -22,6 +27,7 @@ package LeetCode;
 			}
 			if (ptr == null) {
 				ptr = new ListNode(((val + carry) % 10));
+				first = ptr;
 			} 
 			else {
 				ptr.next = new ListNode(((val + carry) % 10));
@@ -31,11 +37,7 @@ package LeetCode;
 				carry = 1;
 			} 
 			else {
-			carry = 0;
-			}
-			if (init) { // get head of LinkedList
-				first = ptr;
-				init = false;
+				carry = 0;
 			}
 			if (l1 != null) {
 				l1 = l1.next;
